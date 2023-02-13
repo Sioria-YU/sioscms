fdaabe4597b4ab3c6803472615a4c6988ebb2055
sioria <chfur12@gmail.com>
2023-02-13 13:41:49 +0900

    최초커밋

--- README.md
+++
@@ -1,2 +0,0 @@
-# sioscms
-SpringFramework, SpringBoot, SpringMVC, SpringDataJPA를 사용하는 Web프로젝트
---
+++ src/main/java/com/example/sioscms/SioscmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sioscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SioscmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SioscmsApplication.class, args);
+    }
+
+}
