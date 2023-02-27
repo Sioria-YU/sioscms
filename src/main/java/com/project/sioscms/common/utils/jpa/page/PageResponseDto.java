91b65bc2b616133c9ce532d0ce82dc5966d700db
sioria <chfur12@gmail.com>
2023-02-27 11:45:16 +0900

    1.pageResponseDto 코멘트 추가

--- src/main/java/com/project/sioscms/common/utils/jpa/page/PageResponseDto.java
+++ src/main/java/com/project/sioscms/common/utils/jpa/page/PageResponseDto.java
@@ -8,11 +8,11 @@ import lombok.Setter;
 @Setter
 @Builder
 public class PageResponseDto {
-    private long totalCount;
-    private int totalPageSize;
-    private int pageNumber;
-    private int pageOffset;
-    private int pageSize;
-    private boolean isNext = false;
-    private boolean isPrev = false;
+    private long totalCount;    //총 컨텐츠 개수
+    private int totalPageSize;  //총 페이지 사이즈
+    private int pageNumber;     //현재 페이지 번호
+    private int pageOffset;     //한 페이지에 표시할 콘텐츠 개수
+    private int pageSize;       //한번에 표시할 페이지 개수
+    private boolean isNext = false; //다음 페이지 목록 여부
+    private boolean isPrev = false; //이전 페이지 목록 여부
 }
